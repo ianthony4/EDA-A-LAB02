e6b26df8a1347ca8e203322da4a687546fb54d6b
- <->
2023-05-19 08:11:13 -0500

    empezando la rotacion

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -5,9 +5,10 @@ public class Ejercicio2{
 		int d = 2;
 		int aux;
 		for(int i = 0; i < arrayIzquierda; i++){
-			
+			//Colocamos en nuestro auxiliar el elemento del arreglo
+			aux = A[i];
 			if(i - d >= 0){
-				
+				arrayIzquierda[i-d] = aux;
 			}
 		}
 	}	
