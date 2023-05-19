34197f607bfd073245a5d5e1946d2a77c3a2b26e
- <->
2023-05-19 08:27:31 -0500

    Corrigiendo en el nombramiento de array's , se corrgio el numero de reiteraciones

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,21 +1,21 @@
 public class Ejercicio2{
-	public int[] rotarArrayIzquierda(int[] A){
-		int[] arrayIzquierda = new int[A.length];
+	public int[] rotarIzquierdaArray(int[] A){
+		int[] Aiz = new int[A.length];
 		//caso prueba
 		int d = 2;
 		int aux;
-		for(int i = 0; i < arrayIzquierda; i++){
+		for(int i = 0; i < Aiz.length; i++){
 			//Colocamos en nuestro auxiliar el elemento del arreglo
 			aux = A[i];
 			//Cuando estemos aptos para empezar la rotacion se hara
 			if(i - d >= 0){
-				arrayIzquierda[i-d] = aux;
+				Aiz[i-d] = aux;
 			}else{
 				//En el caso que este apto, se ira colocando los primero elementos
 				// adelante
-				arrayIzquierda[arrayIzquierda.length - Math.abs(i-d)] = aux;
+				Aiz[Aiz.length - Math.abs(i-d)] = aux;
 			}
 		}	
-		return arrayIzquierda;
+		return Aiz;
 	}
 }
